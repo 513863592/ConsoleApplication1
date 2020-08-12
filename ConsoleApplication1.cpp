cb3656fe4a2120f6a66124d607d05df01244504b
513863592 <69582059+513863592@users.noreply.github.com>
2020-08-12 14:26:38 +0100

    Update ConsoleApplication1.cpp
    111

--- ConsoleApplication1.cpp
+++ ConsoleApplication1.cpp
@@ -1,10 +1,6 @@
 #include <iostream>
 #include <map>
-#include <Vector>
-
 using namespace std;	
-
-
 int main() {
 	int t = 0;
 	cin >> t;
@@ -14,12 +10,11 @@ int main() {
 	int la = 0, sm = 0;
 	int result = 0;
 	int total = 0;
-	int output = 0;
+	int output = 1;
 	map <int, int> ABC;
 	while(t > 0){
 		cin >> n;
 		half = n / 2;
-
 		while(n > 0){
 			cin >> a;
 			cin >> b;
@@ -31,31 +26,29 @@ int main() {
 		map<int, int>::iterator iter;
 		for (int j = 1; j <= half; j++) {
 			for (iter = ABC.begin(); iter != ABC.end(); iter++) {
-
 			if (((iter->second))/10 == j) {
 				if (((iter->second ) % 10) == 0) {
-	
 					sm = iter->first;
-
 				}
 			}
 			if (((iter->second) - 1) / 10 == j) {
 				if (((iter->second-1) % 10) == 0){
-	
 				la = iter->first;
-		
 				}
 			}
 			result = la - sm;
 			if (result > total) {
 				total = result;
-				output = j;
-			}
+				if (result = total) {
+					if (j < output) {
+						output = j;
+					}
+				}
+						}
 			}
 		}
 			t--;
 	}
-
 	cout << output << endl;
 	return 0;
 }
