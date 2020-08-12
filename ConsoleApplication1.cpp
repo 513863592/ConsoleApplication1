9ddf6247c1a27041b2293cb216088d6e4029b35c
513863592 <69582059+513863592@users.noreply.github.com>
2020-08-12 14:08:57 +0100

    Update ConsoleApplication1.cpp
    number 3

--- ConsoleApplication1.cpp
+++ ConsoleApplication1.cpp
@@ -1,14 +1,61 @@
 #include <iostream>
-#include<set>
-#include<unordered_map>
-#include<map>
-#include<unordered_set>
-#include<vector>
-#include<stack>
+#include <map>
+#include <Vector>
+
+using namespace std;	
 
 
-int main()
-{
-    std::cout << "Hello World!\n";
-    1111
+int main() {
+	int t = 0;
+	cin >> t;
+	int n;
+	int a = 0, b = 0, c = 0;
+	int half = 0;
+	int la = 0, sm = 0;
+	int result = 0;
+	int total = 0;
+	int output = 0;
+	map <int, int> ABC;
+	while(t > 0){
+		cin >> n;
+		half = n / 2;
+
+		while(n > 0){
+			cin >> a;
+			cin >> b;
+			cin >> c;
+			b = b * 10 + c;
+			ABC.insert(pair<int, int>(a, b));
+			n--;
+		}
+		map<int, int>::iterator iter;
+		for (int j = 1; j <= half; j++) {
+			for (iter = ABC.begin(); iter != ABC.end(); iter++) {
+
+			if (((iter->second))/10 == j) {
+				if (((iter->second ) % 10) == 0) {
+	
+					sm = iter->first;
+
+				}
+			}
+			if (((iter->second) - 1) / 10 == j) {
+				if (((iter->second-1) % 10) == 0){
+	
+				la = iter->first;
+		
+				}
+			}
+			result = la - sm;
+			if (result > total) {
+				total = result;
+				output = j;
+			}
+			}
+		}
+			t--;
+	}
+
+	cout << output << endl;
+	return 0;
 }
